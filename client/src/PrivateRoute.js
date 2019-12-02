d38ff3b0fdc5a555a268f033d043d2f62d424328
Will Gutierrez <will.gutierrez@gmail.com>
2019-12-02 14:15:30 -0800

    create PrivateRoute.js to wrap protected routes

---
+++ client/src/PrivateRoute.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location, ...props }) =>
+        user ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    area_code: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+  }),
+  component: PropTypes.func.isRequired // eslint-disable-line react/forbid-prop-types
+};
+
+PrivateRoute.defaultProps = {
+  user: {}
+};
+
+const mapStateToProps = state => ({
+  user: state.user.user
+});
+
+export default connect(mapStateToProps, {})(PrivateRoute);
